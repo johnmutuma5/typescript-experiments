789b9837f9776a6a37032b2518778ef2b8337e34
John Mutuma (Thoguo_) <johnmutuma5@gmail.com>
2019-12-07 17:38:38 +0300

    add docs and add from creator

--- src/observable/base/observable.ts
+++ src/observable/base/observable.ts
@@ -4,10 +4,23 @@ interface Observer<T> {
   onCompleted?: (val?: any) => any,
 }
 
+/**
+ *                                                                                                 
+ * .oPYo. 8                                        8      8          .oPYo. 8                      
+ * 8    8 8                                        8      8          8    8 8                      
+ * 8    8 8oPYo. .oPYo. .oPYo. oPYo. o    o .oPYo. 8oPYo. 8 .oPYo.   8      8 .oPYo. .oPYo. .oPYo. 
+ * 8    8 8    8 Yb..   8oooo8 8  `' Y.  .P .oooo8 8    8 8 8oooo8   8      8 .oooo8 Yb..   Yb..   
+ * 8    8 8    8   'Yb. 8.     8     `b..d' 8    8 8    8 8 8.       8    8 8 8    8   'Yb.   'Yb. 
+ * `YooP' `YooP' `YooP' `Yooo' 8      `YP'  `YooP8 `YooP' 8 `Yooo'   `YooP' 8 `YooP8 `YooP' `YooP' 
+ * :.....::.....::.....::.....:..::::::...:::.....::.....:..:.....::::.....:..:.....::.....::.....:
+ * ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ * ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ *
+ */
 export class Observable<T> {
   private _subscribe: any;
 
-  constructor(subscribe?: (obs: Observer<T>) => any) {
+  constructor(subscribe?: (obs: Observer<T>) => () => any) {
     if(!!subscribe)
       this._subscribe = subscribe;
   }
@@ -23,6 +36,10 @@ export class Observable<T> {
       return this._subscribe(onNext); // if it's a subscriber object
   }
 
+  /**
+   * @description create an observable from arguments
+   *
+   */
   static of<U>(...args: U[]): Observable<U> {
     return new Observable<U>((obs: Observer<U>) => {
       args.forEach((arg: U) => {
@@ -35,10 +52,34 @@ export class Observable<T> {
       obs.onCompleted && obs.onCompleted();
 
       return () => obs = ({ // unsubscribe
-        onNext: () => {},
-        onError: () => {},
-        onCompleted: () => {},
+        onNext      : () => {},
+        onError     : () => {},
+        onCompleted : () => {},
       });
+    });
+  }
+
+  /**
+   * @description create an observable from an iterable object eg array
+   *
+   */
+  static from<U>(values: any): Observable<U> {
+    return new Observable<U>((obs: Observer<U>) => {
+      for(let val of values)
+        try{
+          obs.onNext(val);
+        } catch(error) {
+          obs.onError && obs.onError(error);
+        }
+
+      obs.onCompleted && obs.onCompleted();
+      return () => { // unsubscribe
+        obs = {
+          onNext      : () => {},
+          onError     : () => {},
+          onCompleted : () => {},
+        }
+      };
     });
   }
 }
--- src/observable/index.ts
+++ src/observable/index.ts
@@ -1,4 +1,34 @@
 import { Observable } from "./base/observable";
 
-const obs$: Observable<number> = Observable.of<number>(1, 2, 3, 4, 5, 6)
+/**
+ *                                                                                
+ * .oPYo. 8                                        8      8                   d'b 
+ * 8    8 8                                        8      8                   8   
+ * 8    8 8oPYo. .oPYo. .oPYo. oPYo. o    o .oPYo. 8oPYo. 8 .oPYo.    .oPYo. o8P  
+ * 8    8 8    8 Yb..   8oooo8 8  `' Y.  .P .oooo8 8    8 8 8oooo8    8    8  8   
+ * 8    8 8    8   'Yb. 8.     8     `b..d' 8    8 8    8 8 8.        8    8  8   
+ * `YooP' `YooP' `YooP' `Yooo' 8      `YP'  `YooP8 `YooP' 8 `Yooo' 88 `YooP'  8   
+ * :.....::.....::.....::.....:..::::::...:::.....::.....:..:.....:..::.....::..::
+ * :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ * :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ */
+const obs$: Observable<number> = Observable.of<number>(1, 2, 3, 4, 5, 6);
 obs$.subscribe(console.log);
+
+
+/**
+ *                                                                                    
+ * .oPYo. 8                                                  d'b                      
+ * 8    8 8                                                  8                        
+ * 8    8 8oPYo. .oPYo. .oPYo. oPYo. o    o .oPYo. oPYo.    o8P  oPYo. .oPYo. ooYoYo. 
+ * 8    8 8    8 Yb..   8oooo8 8  `' Y.  .P 8oooo8 8  `'     8   8  `' 8    8 8' 8  8 
+ * 8    8 8    8   'Yb. 8.     8     `b..d' 8.     8         8   8     8    8 8  8  8 
+ * `YooP' `YooP' `YooP' `Yooo' 8      `YP'  `Yooo' 8     88  8   8     `YooP' 8  8  8 
+ * :.....::.....::.....::.....:..::::::...:::.....:..::::..::..::..:::::.....:..:..:..
+ * :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ * :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+ *
+ */
+
+const obsFrom$: Observable<string> = Observable.from(['Hello', 'dear', 'world', '!!']);
+obsFrom$.subscribe(console.log);
